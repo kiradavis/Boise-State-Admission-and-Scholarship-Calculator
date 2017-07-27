1a51fb6cd5467f4aa9f6a5361174308b2256531b
kiradavis <kiradavis695@boisestate.edu>
2017-07-27 13:54:24 -0600

    Initial

---
+++ boise-state-admissions-calculator.php
@@ -0,0 +1,238 @@
+<?php
+/*
+*	Plugin Name: Boise State Admission & Scholarship Calculators
+*	Description: Plugin to handle the admission and scholarship calculators for admissions.boisestate.edu. [calc type=""] & [calc type="scholarship"]
+*	Version: 0.1
+*	Author: Kira Davis & David Lentz
+*	Author URI: https://webguide.boisestate.edu/
+*	Based on the impressive work at https://jsfiddle.net/ValentinH/954eve2L/
+*	See https://github.com/angular-slider/angularjs-slider/blob/master/README.md for useful info on how to work with rz-slider 
+*/
+
+function my_assets() {
+	// register AngularJS
+	wp_register_script( 'angular-core', 'https://ajax.googleapis.com/ajax/libs/angularjs/1.6.5/angular.min.js', array(), null, false );
+	
+	// register CSS
+	wp_register_style( 'calc-css', plugins_url('style.css',__FILE__ ) );
+	wp_register_style( 'slider-css', 'https://rawgit.com/rzajac/angularjs-slider/master/dist/rzslider.css' );
+	
+	// register scripts
+	wp_register_script( 'calc-js', plugins_url('script.js',__FILE__ ) );
+	wp_register_script( 'slider-js', 'https://rawgit.com/rzajac/angularjs-slider/master/dist/rzslider.js' );
+	wp_register_script( 'bootstrap-js', 'https://cdnjs.cloudflare.com/ajax/libs/angular-ui-bootstrap/0.14.3/ui-bootstrap-tpls.js' );
+	
+	// enqueue everything
+	wp_enqueue_script( 'angular-core' );
+	wp_enqueue_style( 'calc-css' );
+	wp_enqueue_style( 'slider-css' );
+	wp_enqueue_script( 'calc-js' );
+	wp_enqueue_script( 'slider-js' );
+  	wp_enqueue_script( 'bootstrap-js' );
+}
+
+add_action( 'wp_enqueue_scripts', 'my_assets' );
+
+function calculator_html($atts = array(), $content = null, $tag) {
+    shortcode_atts(array(
+        'type' => 'scholarship'
+    ), $atts);
+	
+	$str .= '<div ng-app="calculatorApp" id="slider-body">';
+		$str .= '<div ng-controller="calculatorCtrl" class="wrapper">';
+			$str .= '<div class="row all-options">';
+      			$str .= '<div class="one_fourth sliders" id="slider-gpa" style="height: 600px !important">';
+					$str .= '<small>GPA</small><br>';	
+					$str .= '<rzslider rz-slider-model="sliderGPA.value" rz-slider-options="sliderGPA.options"></rzslider>';
+				$str .= '</div>';
+				$str .= '<div class="one_fourth last sliders" id="slider1" style="height: 600px !important">';	
+					$str .= '<small>SAT - ACT</small><br>';
+					$str .= '<rzslider rz-slider-model="sliderScore.value" rz-slider-options="sliderScore.options"></rzslider><br>';
+				$str .= '</div>';
+				$str .= '<div class="one_half" id="slider-options">';
+					$str .= 'Please use the scales on the left to select your GPA and the highest of either your SAT or ACT score.<br><br>';
+					$str .= '<label class="field-title">GPA:</label><br>';
+					$str .= '<input type="number" ng-model="sliderGPA.value" max="4" min="2" step="0.01"/><br>';
+  					$str .= '<label class="field-title">SAT or ACT equivalent Score:</label><br>';
+					$str .= '<input type="number" ng-model="sliderScore.value" max="36" min="11" /><br>';
+					if ($atts['type'] == 'scholarship') {
+						$str .= '<label class="field-title">Participating States:';
+						$str .= '<select name="state" ng-model="state" >';
+						$str .= '<option value="AK" selected="selected">Alaska</option>';
+						$str .= '<option value="AZ">Arizona</option>';
+						$str .= '<option value="CA">California</option>';
+						$str .= '<option value="CO">Colorado</option>';
+						$str .= '<option value="HI">Hawaii</option>';
+						$str .= '<option value="MT">Montana</option>';
+						$str .= '<option value="NV">Nevada</option>';
+						$str .= '<option value="NM">New Mexico</option>';
+						$str .= '<option value="ND">North Dakota</option>';
+						$str .= '<option value="OR">Oregon</option>';
+						$str .= '<option value="SD">South Dakota</option>';
+						$str .= '<option value="UT">Utah</option>';
+						$str .= '<option value="WA">Washington</option>';
+						$str .= '<option value="WY">Wyoming</option>';
+						$str .= '<option value="PT">US Pacific Territories and Freely Associated States</option>';
+						$str .= '<option value="NA">My state is not listed</option>';
+						$str .= '</select></label><br><br>';
+						$str .= '<label>Are you a US Citizen or Permanent Resident?';
+						$str .= '<input type="checkbox" ng-model="chkselect" ng-click="checkValidation()" checked="true">';
+						$str .= '</label>';
+						$str .= '<span ng-show="validationmsg">International Students are considered for the GEM nonresident scholarship program. You must submit all admission materials by the December 15th scholarship deadline to qualify for consideration.<br></span><br>';
+						$str .= 'Press calculate after setting sliders to check your eligibility<br><br>';
+						$str .= '<input type="button" value="Calculate" ng-click="nonresidentCalculate()"><br>';
+						$str .= '<em>';
+						$str .= '<span ng-show="I">' . get_option( 'message_I' ) . '</span>' 
+                          		. '<span ng-show="II">' . get_option( 'message_II' ) . '</span>'
+                          		. '<span ng-show="III">' . get_option( 'message_III' ) . '</span>' 
+                          		. '<span ng-show="IV">' . get_option( 'message_IV' ) . '</span>';
+						$str .= '</em>';
+					} else { 
+						$str .= '<br><input type="button" value="Calculate" ng-click="calculate()"><br>';
+						$str .= '<em>{{msg}}</em>';
+					}
+				$str .= '</div>'; // end one half
+			$str .= '</div>'; // end options 
+		$str .= '</div>'; // end controller
+	$str .= '</div>'; // end app
+
+	return $str;
+}
+
+add_shortcode('calc', 'calculator_html');
+
+//-----------------------------------------------------
+// Admin Page Settings
+//-----------------------------------------------------
+ 
+add_action('admin_menu', 'boise_state_ac_admin_settings');
+add_action('admin_menu', 'boise_state_sc_admin_settings');
+
+function boise_state_ac_admin_settings() {
+	$parent_slug = 'options-general.php';
+    $page_title = 'Boise State Admission Calculator Settings';
+    $menu_title = 'Admission Calculator';
+    $capability = 'edit_posts';
+    $menu_slug = 'boise_state_ac_options';
+    $function = 'boise_state_ac_admin_settings_page_display';
+    // $icon_url = '';
+    // $position = 80;
+
+    //add_submenu_page( $page_title, $menu_title, $capability, $menu_slug, $function, $icon_url, $position );
+	add_submenu_page( $parent_slug, $page_title, $menu_title, $capability, $menu_slug, $function );
+}
+
+function boise_state_sc_admin_settings() {
+	$parent_slug = 'options-general.php';
+    $page_title = 'Boise State Scholarship Calculator Settings';
+    $menu_title = 'Scholarship Calculator';
+    $capability = 'edit_posts';
+    $menu_slug = 'boise_state_sc_options';
+    $function = 'boise_state_sc_admin_settings_page_display';
+
+    //add_submenu_page( $page_title, $menu_title, $capability, $menu_slug, $function, $icon_url, $position );
+	add_submenu_page( $parent_slug, $page_title, $menu_title, $capability, $menu_slug, $function );
+}
+
+//-----------------------------------------------------
+// Settings Fields
+//-----------------------------------------------------
+/* Admission */
+function low_score_field() { 
+	$low_score = get_option( 'low_score' );
+	echo wp_editor( $low_score, 'low_score', array('textarea_content' => 'low_score') );
+}
+function mid_score_field() { 
+	$mid_score = get_option( 'mid_score' );
+	echo wp_editor( $mid_score, 'mid_score', array('textarea_content' => 'mid_score') );
+}
+function high_score_field() { 
+	$high_score = get_option( 'high_score' );
+	echo wp_editor( $high_score, 'high_score', array('textarea_content' => 'high_score') );
+}
+/* Scholarship */
+function message_I_field() { 
+	$message_I = get_option( 'message_I' );
+	echo wp_editor( $message_I, 'message_I', array('textarea_content' => 'message_I') );
+}
+function message_II_field() { 
+	$message_II = get_option( 'message_II' );
+	echo wp_editor( $message_II, 'message_II', array('textarea_content' => 'message_II') );
+}
+function  message_III_field() { 
+	$message_III = get_option( 'message_III' );
+	echo wp_editor( $message_III, 'message_III', array('textarea_content' => 'message_III') );
+}
+function  message_IV_field() { 
+	$message_IV = get_option( 'message_IV' );
+	echo wp_editor( $message_IV, 'message_IV', array('textarea_content' => 'message_IV') );
+}
+
+function boise_state_ac_display_theme_panel_fields() {
+	add_settings_section("ac-section", "Admissions Calculator", null, "ac-options");
+	add_settings_section("sc-section", "Scholarship Calculator", null, "sc-options");
+
+	/* Admissions Settings */
+	add_settings_field("low_score", "Low score message:", "low_score_field", "ac-options", "ac-section");
+	add_settings_field("mid_score", "Middle score message:", "mid_score_field", "ac-options", "ac-section");
+	add_settings_field("high_score", "High score message:", "high_score_field", "ac-options", "ac-section");
+	
+	/* Scholarship Settings */
+	add_settings_field("message_I", "Message I (gpa or act too low: no scholarship)", "message_I_field", "sc-options", "sc-section");
+	add_settings_field("message_II", "Message II (gpa and act in middle)", "message_II_field", "sc-options", "sc-section");
+	add_settings_field("message_III", "Message III (gpa and act high: gem scholarship)", "message_III_field", "sc-options", "sc-section");
+	add_settings_field("message_IV", "Message IV (non-wue: too low for gem)", "message_IV_field", "sc-options", "sc-section");
+
+	/* Register All Settings */
+	register_setting("ac-section", "low_score");
+	register_setting("ac-section", "mid_score");
+	register_setting("ac-section", "high_score");
+	register_setting("sc-section", "message_I");
+	register_setting("sc-section", "message_II");
+	register_setting("sc-section", "message_III");
+	register_setting("sc-section", "message_IV");
+}
+
+add_action("admin_init", "boise_state_ac_display_theme_panel_fields");
+
+//-----------------------------------------------------
+// Admin Page Display
+//-----------------------------------------------------
+
+function boise_state_ac_admin_settings_page_display() {
+	// check user capabilities
+	if ( ! current_user_can( 'manage_options' ) ) {
+		wp_die('You do not have sufficient permissions to access this page.');
+	}
+	?>
+	<div class="wrap">
+	<h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
+	    <form method="post" action="options.php" enctype="multipart/form-data">
+	        <?php
+				settings_fields("ac-section");
+				do_settings_sections("ac-options"); 			
+				submit_button(); 
+	        ?>          
+		</form>
+	</div>
+	<?php
+}
+
+function boise_state_sc_admin_settings_page_display() {
+	// check user capabilities
+	if ( ! current_user_can( 'manage_options' ) ) {
+		wp_die('You do not have sufficient permissions to access this page.');
+	}
+	?>
+	<div class="wrap">
+	<h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
+	    <form method="post" action="options.php" enctype="multipart/form-data">
+	        <?php
+				settings_fields("sc-section");
+				do_settings_sections("sc-options"); 				
+				submit_button(); 
+	        ?>          
+		</form>
+	</div>
+	<?php
+}
---
+++ script.js
@@ -0,0 +1,117 @@
+
+var app = angular.module('calculatorApp', ['rzModule', 'ui.bootstrap']);
+
+app.controller('calculatorCtrl', function ($scope, $rootScope, $timeout, $modal) {
+
+$scope.msg = "";
+$scope.chkselect = true;
+$scope.state = "AK";
+
+// Are you a US Citizen or Permanent Resident?
+$scope.checkValidation = function() {
+	var chkselect = $scope.chkselect;
+	if (chkselect == false) { $scope.validationmsg = true; } 
+	else { $scope.validationmsg = false; }
+}
+
+// Calculate
+$scope.calculate = function() {
+	var rawScore = ((0.64091*($scope.sliderGPA.value))-0.28946+(0.036599*($scope.sliderScore.value))-1.6)/0.019636;
+	
+	if (rawScore >= 34.5) {
+		$scope.msg = get_option('high_score');
+	} else if (rawScore >= 29.5) {
+		$scope.msg = get_option('mid_score');
+	} else {
+		$scope.msg = get_option('low_score');
+	}
+}
+
+$scope.I = false;
+$scope.II = false;
+$scope.III = false;
+$scope.IV = false;
+
+$scope.nonresidentCalculate = function() {
+	/*
+	I. gpa or act too low - no scholarship
+	II. wue: gpa and act in middle - wue scholarship	
+	III. gpa and act high - gem scholarship
+	IV. non wue: gpa and act in middle - too low for gem 
+	*/
+	
+	$scope.I = false;
+	$scope.II = false;
+	$scope.III = false;
+	$scope.IV = false;
+	
+	//gpa or act too low: no scholarship
+	if ($scope.sliderGPA.value < 3.2 || $scope.sliderScore.value < 21 ) {
+		$scope.I = true;
+	//gpa and act high: gem scholarship
+	} else if ($scope.sliderGPA.value >= 3.6 && $scope.sliderScore.value >= 26) {
+		$scope.III = true;
+	//gpa and act in middle
+	} else { 
+		if ($scope.state == 'NA' || $scope.chkselect == false) { //$scope.chkselect == false
+			// non-wue: too low for gem
+			$scope.IV = true;
+		} else {
+			// wue: wue scholarship
+			$scope.II = true;
+		}
+	}
+}
+
+//Vertical sliders
+$scope.sliderScore = {
+    value: 11,
+    options: {
+        floor: 11,
+        ceil: 36,
+        vertical: true, 
+        showTicksValues: true,
+		step: 1,
+        stepsArray: [
+            {value: 11, legend: '560-620'}, 
+            {value: 12, legend: '630-710'}, 
+            {value: 13, legend: '720-750'}, 
+            {value: 14, legend: '760-800'}, 
+            {value: 15, legend: '810-850'}, 
+            {value: 16, legend: '860-890'}, 
+            {value: 17, legend: '900-930'}, 
+            {value: 18, legend: '940-970'}, 
+            {value: 19, legend: '980-1010'}, 
+            {value: 20, legend: '1020-1050'}, 
+            {value: 21, legend: '1060-1090'}, 
+            {value: 22, legend: '1100-1120'}, 
+            {value: 23, legend: '1130-1150'}, 
+            {value: 24, legend: '1160-1190'}, 
+            {value: 25, legend: '1200-1230'}, 
+            {value: 26, legend: '1240-1270'}, 
+            {value: 27, legend: '1280-1300'}, 
+            {value: 28, legend: '1310-1340'}, 
+           	{value: 29, legend: '1350-1380'}, 
+           	{value: 30, legend: '1390-1410'}, 
+           	{value: 31, legend: '1420-1440'}, 
+           	{value: 32, legend: '1450-1480'}, 
+           	{value: 33, legend: '1490-1510'}, 
+           	{value: 34, legend: '1520-1550'}, 
+           	{value: 35, legend: '1560-1590'}, 
+           	{value: 36, legend: '1600'}
+        ]
+    }
+};
+
+//GPA Slider
+$scope.sliderGPA = {
+    value: 2.00,
+    options: {
+        floor: 2.00,
+        ceil: 4.00,
+		step: 0.01,
+		precision: 3,
+        vertical: true
+    }
+};
+});
