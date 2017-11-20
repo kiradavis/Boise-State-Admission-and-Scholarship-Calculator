0408001aaa8e22b203159cefe50feb72a0c6c9a0
kiradavis <kiradavis695@boisestate.edu>
2017-11-20 09:11:54 -0700

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -4,7 +4,7 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
     die;
 }
 
-$options = array("message_1", "message_II", "message_III", "message_IV", "low_score", "mid_score", "high_score"); 
+$options = array("message_I", "message_II", "message_III", "message_IV", "low_score", "mid_score", "high_score"); 
 
 for ($i = 0; $i < $options.count(); $i++) {
 	delete_site_option($options[$i]);
